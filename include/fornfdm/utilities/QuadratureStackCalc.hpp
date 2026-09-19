df465affc31d95a51cf3936ea43106d7de9eee8c
Jack Feds <jackf10oh@gmail.com>
2026-09-19 17:44:44 -0400

    took quadrature calculation into a free function as well. used inside of StackCalc version. still avoiding allocations for smaller cases.

--- include/fornfdm/utilities/QuadratureStackCalc.hpp
+++ include/fornfdm/utilities/QuadratureStackCalc.hpp
@@ -1,14 +1,13 @@
 // QuadratureStackCalc.hpp
 //
-// reverses the Fornberg algorithm to compute weights for 
-// a definite integral on [x_left, x_right]. 
-// computes from nthIntegral repeated integral to 
-// numIntegralsMax repeated integrals. 
-// most commonly just going 1 repeated integral to 1 repeated integral.
-//
-// no free function since the memory usage is pretty strange
+// the quadrature analog of FornbergStackCalc
 //
 // JAF 8/8/2026
+
+#include<array>
+#include<cmath>
+#include<cassert>
+#include "../types.hpp"
 
 #ifndef FORNFDM_UTILS_QUADRATURESTACKCALC_H
 #define FORNFDM_UTILS_QUADRATURESTACKCALC_H
@@ -16,88 +15,52 @@
 namespace fornfdm{
   namespace utils{
 
-template<std::size_t numNodes_, std::size_t nthIntegral_, class ScalarType_, std::size_t numIntegralsMax_=nthIntegral_>
+template<std::size_t M, std::size_t N, typename ScalarType_ = fornfdm::Scalar>
 class QuadratureStackCalc
 {
-  private:
+  public:
     // Type Defs ------------ 
     using ScalarType = ScalarType_;
 
     // Member Data ---------------- 
-    static constexpr std::size_t nthIntegral = nthIntegral_; 
-    static constexpr std::size_t numIntegralsMax = numIntegralsMax_; 
-    static constexpr std::size_t numNodes = numNodes_; 
+    static constexpr std::size_t numNodesMax = M; 
+    static constexpr std::size_t nthIntegral = N; 
+
+  private:
+    using ArrayType = std::array<ScalarType, M*(N+1)>; 
+    ArrayType m_arr;          // single allocation of memory rows*cols big 
     std::size_t m_nodes_used = 0;
-    std::array<ScalarType, numNodes * (numNodes+numIntegralsMax-nthIntegral)> m_buff_left;
-    std::array<ScalarType, numNodes * (numNodes+numIntegralsMax-nthIntegral)> m_buff_right;
 
   public:
     // Constructors + Destructor =================
     QuadratureStackCalc()=default;
     QuadratureStackCalc(const QuadratureStackCalc& other)=default;
+
+    template<typename Iter>
+    QuadratureStackCalc(ScalarType x_left, ScalarType x_right, Iter start, Iter end)
+    { 
+      calculate(x_left,x_right,start,end); 
+    }
+
     ~QuadratureStackCalc()=default;
 
-    // Member Functions ------------
-    const auto& getArray() const { return m_buff_right; }
+    // Member Functions ==================================== 
 
-    template<class InputIter, class = std::enable_if_t<std::is_base_of_v<std::random_access_iterator_tag, typename std::iterator_traits<InputIter>::iterator_category>>>
-    void calculate(ScalarType x_left, ScalarType x_right, InputIter node_start)
+    // Const getter to stored weights 
+    const auto& getArray() const { return m_arr; }
+    auto getNumNodesUsed() const { return m_nodes_used; }  
+
+    // Updates m_arr to contain weights up to nth repeated integral
+    template<typename Iter>
+    void calculate(ScalarType x_left, ScalarType x_right, Iter start, Iter end)
     {
-      // notation: kth weight using n nodes for mth repeated integral
-      // I(m,n,k)
-
-      // Initialize first column I(nthIntegral,1,0), I(nthIntegral+1,1,0), ..., I(numIntegralsMax+numNodes,1,0) 
-      std::size_t fact = 1;
-      ScalarType xr_powm = std::pow(x_right - *node_start,nthIntegral);
-      ScalarType xl_powm = std::pow(x_left - *node_start,nthIntegral);
-      for(std::size_t m=0; m < numIntegralsMax+numNodes-nthIntegral; ++m){
-        fact *= nthIntegral + m;
-        m_buff_right[m * numNodes] = xr_powm / fact; 
-        m_buff_left[m * numNodes] = xl_powm / fact;
-        xr_powm *= x_right - *node_start;
-        xl_powm *= x_left - *node_start;
-      }
-
-      ScalarType c1 = 1.0; 
-      ScalarType c2; 
-      ScalarType c3; 
-      // iteratre 2nd until last column 
-      for(std::size_t n=1; n < numNodes; ++n)
-      {
-        c2 = 1.0; 
-        // loop through previous columns 
-        for(std::size_t k=0; k < n; ++k)
-        {
-          c3 = node_start[n] - node_start[k]; 
-          c2 *= c3; 
-          // newest column depends on OLD values of prev column ...
-          if(k == n - 1)
-          {
-            for(std::size_t m=0; m < numIntegralsMax - nthIntegral + numNodes - n; ++m)
-            {
-              // (3.9) --------------------
-              // I(m,n,n)(x) = [(x-a_n)*I(m,n-1,n-1) - (m+1)*I(m,n-1,n)] * (c1/c2)
-              m_buff_right[m*numNodes + n] = ((x_right - node_start[n-1])*m_buff_right[m*numNodes+n-1] - (m+nthIntegral)*m_buff_right[(m+1)*numNodes+n-1])*(c1/c2);
-              m_buff_left[m*numNodes + n] = ((x_left - node_start[n-1])*m_buff_left[m*numNodes+n-1] - (m+nthIntegral)*m_buff_left[(m+1)*numNodes+n-1])*(c1/c2);
-            } 
-          }; 
-          // updates m = nthIntegral, nthIntegral+1, ..., (numIntegralsMax + numNodes - n) th repeated integral 
-          for(std::size_t m=0; m < (numIntegralsMax-nthIntegral+numNodes-n); ++m)
-          {
-            // (3.8) -----------------
-            // I(m,n,k)(x) = [m*I(m+1,n,k-1) - (x-a_n)*I(m,n,k-1)] * (a_n-a_k)
-            m_buff_right[m*numNodes+k] = ((m+nthIntegral) * m_buff_right[(m+1)*numNodes+k] - (x_right-node_start[n]) * m_buff_right[m*numNodes+k]) / c3;
-            m_buff_left[m*numNodes+k] = ((m+nthIntegral) * m_buff_left[(m+1)*numNodes+k] - (x_left-node_start[n]) * m_buff_left[m*numNodes+k]) / c3;
-          }; 
-        }
-        c1 = c2; 
-      }
-      std::transform(
-        m_buff_right.begin(), std::next(m_buff_right.begin(), (numIntegralsMax-nthIntegral+1)*numNodes),
-        m_buff_left.begin(), 
-        m_buff_right.begin(),
-        std::minus<ScalarType>{}
-      );
+      using T = ArrayType::pointer;
+      constexpr std::size_t N = numNodesMax * (nthIntegral + numNodesMax - 1) + 1;
+      // make sure distance(start,end) <= numNodesMax 
+      auto d = std::distance(start,end); 
+      assert((d <= numNodesMax) && "QuadratureStackCalc error: distance(start,end) > numNodesMax");  
+      m_nodes_used = d; 
+      fornfdm::utils::quadrature<Iter,T,N>(start, end, x_left, x_right, N, m_arr.data());
     }
 };
 
--- include/fornfdm/utilities/quadrature.hpp
+++ include/fornfdm/utilities/quadrature.hpp
@@ -1,25 +1,194 @@
 // quadrature.hpp
 //
-// reverse of the Fornberg algorithm do calculate 
+// reverse of the Fornberg algorithm to calculate 
 // numerical integral weights instead of finite difference weights. 
-// note: the # of repeated integrals is descending.
+//
+// note: typically the function returns a 1 past the end
+// of where the result is written to.
+// if the function is given inputs that would exceed the std::array's 
+// it will return the "dest" iterator without advancing it any. 
 //
 // JAF 8/9/2026
+
+#include<array>
+#include<vector>
+#include<iterator>
+#include<cstdint>
+#include<type_traits>
 
 #ifndef FORNFDM_UTILS_QUADRATURE_H
 #define FORNFDM_UTILS_QUADRATURE_H
 
-std::pair<double*, double*> quadrature(
-  const double* node_start, const double* node_end,  
-  double x_left, double x_right, 
-  std::size_t min_m, std::size_t max_m,
-  double* dest_left, double* dest_right 
+namespace fornfdm{
+  namespace utils{
+ 
+template<typename ForwardIn, typename OutputIt>
+OutputIt allocating_quadrature(
+  ForwardIn node_start, ForwardIn node_end,
+  const typename std::iterator_traits<ForwardIn>::value_type& x_left,  
+  const typename std::iterator_traits<ForwardIn>::value_type& x_right,  
+  std::size_t M_repeats, 
+  OutputIt dest 
 )
 {
+  using Scalar = typename std::iterator_traits<ForwardIn>::value_type; 
+  std::size_t num_nodes = std::distance(node_start, node_end);
+  std::vector<Scalar> left_vec(num_nodes * (M_repeats+num_nodes-1) + 1); 
+  std::vector<Scalar> right_vec(num_nodes * (M_repeats+num_nodes-1) + 1);
 
+  // first column gets set to (left_x - a(0)) ^ i / (i!) for i=0,...,M_repeats
+  left_vec[0] = 1;
+  right_vec[0] = 1;
+  std::size_t fact = 1; 
+  Scalar left_exp = 1.0;
+  Scalar right_exp = 1.0;
+  for(auto row_idx = 1; row_idx < M_repeats+num_nodes; ++row_idx)
+  {
+    fact = fact * row_idx;
+    left_exp = left_exp * (x_left - *node_start);
+    right_exp = right_exp * (x_right - *node_start);
+    left_vec[row_idx * num_nodes] = left_exp / fact;
+    right_vec[row_idx * num_nodes] = right_exp / fact;
+  }
 
+  Scalar c1, c2, c3 = 1.0; 
+  std::size_t current_M = std::max(M_repeats, M_repeats + num_nodes - 1);
+  for(auto node=std::next(node_start); node!=node_end; ++node)
+  {
+    std::size_t count = 0;
+    c2 = 1.0;
+    for(auto old_node = node_start; old_node!=node; ++old_node, ++count)
+    {
+      c1 = *node - *old_node;
+      c2 *= c1;
 
-  return {} // returns a std::pair<double*,double*> of 2 "one past the end" output iterators
+      if(std::next(old_node) == node)
+      {
+        std::size_t m = current_M;
+        do
+        {
+          --m;
+          // (3.9) -----------------------
+          std::size_t idx = m * num_nodes + count + 1;
+          left_vec[idx] = c3 * ((x_left - *old_node) * left_vec[idx-1] - m * left_vec[idx+num_nodes-1]) / c2;
+          right_vec[idx] = c3 * ((x_right - *old_node) * right_vec[idx-1] - m * right_vec[idx+num_nodes-1]) / c2;
+        } while (m != 0);
+      }
+      std::size_t m = current_M;
+      do
+      {
+        --m;
+        // (3.8) -----------------------
+        std::size_t idx = m * num_nodes + count;
+        left_vec[idx] = (m * left_vec[idx + num_nodes] - (x_left - *node)* left_vec[idx]) / c1;
+        right_vec[idx] = (m * right_vec[idx + num_nodes] - (x_right - *node) * right_vec[idx]) / c1;
+      } while (m != 0);
+    }
+    c3 = c2;
+    if(current_M > M_repeats)
+    {
+      --current_M;
+    }
+  }
+  // return the actual difference of both vectors. 
+  auto end = std::next(right_vec.cbegin(), num_nodes * (M_repeats+1));
+  return std::transform(
+    right_vec.cbegin(), end,
+    left_vec.cbegin(), 
+    dest, 
+    [](const Scalar& r, const Scalar& l){ return r-l; }
+  );
 }
+
+template<typename ForwardIn, typename OutputIt, std::size_t N=100>
+OutputIt quadrature(
+  ForwardIn node_start, ForwardIn node_end,
+  const typename std::iterator_traits<ForwardIn>::value_type& x_left,  
+  const typename std::iterator_traits<ForwardIn>::value_type& x_right,  
+  std::size_t M_repeats, 
+  OutputIt dest 
+)
+{
+  using Scalar = typename std::iterator_traits<ForwardIn>::value_type; 
+
+  std::size_t num_nodes = std::distance(node_start, node_end);
+  if((num_nodes * (M_repeats+num_nodes-1) + 1) > N){
+    return allocating_quadrature<ForwardIn,OutputIt>(
+      node_start,node_end,
+      x_left, x_right,
+      M_repeats,
+      dest
+    );
+  }
+
+  std::array<Scalar,N> left_arr{}; 
+  std::array<Scalar,N> right_arr{};
+
+  // first column gets set to (left_x - a(0)) ^ i / (i!) for i=0,...,M_repeats
+  left_arr[0] = 1;
+  right_arr[0] = 1;
+  std::size_t fact = 1; 
+  Scalar left_exp = 1.0;
+  Scalar right_exp = 1.0;
+  for(auto row_idx = 1; row_idx < M_repeats+num_nodes; ++row_idx)
+  {
+    fact = fact * row_idx;
+    left_exp = left_exp * (x_left - *node_start);
+    right_exp = right_exp * (x_right - *node_start);
+    left_arr[row_idx * num_nodes] = left_exp / fact;
+    right_arr[row_idx * num_nodes] = right_exp / fact;
+  }
+
+  Scalar c1, c2, c3 = 1.0; 
+  std::size_t current_M = std::max(M_repeats, M_repeats + num_nodes - 1);
+  for(auto node=std::next(node_start); node!=node_end; ++node)
+  {
+    std::size_t count = 0;
+    c2 = 1.0;
+    for(auto old_node = node_start; old_node!=node; ++old_node, ++count)
+    {
+      c1 = *node - *old_node;
+      c2 *= c1;
+
+      if(std::next(old_node) == node)
+      {
+        std::size_t m = current_M;
+        do
+        {
+          --m;
+          // (3.9) -----------------------
+          std::size_t idx = m * num_nodes + count + 1;
+          left_arr[idx] = c3 * ((x_left - *old_node) * left_arr[idx-1] - m * left_arr[idx+num_nodes-1]) / c2;
+          right_arr[idx] = c3 * ((x_right - *old_node) * right_arr[idx-1] - m * right_arr[idx+num_nodes-1]) / c2;
+        } while (m != 0);
+      }
+      std::size_t m = current_M;
+      do
+      {
+        --m;
+        // (3.8) -----------------------
+        std::size_t idx = m * num_nodes + count;
+        left_arr[idx] = (m * left_arr[idx + num_nodes] - (x_left - *node)* left_arr[idx]) / c1;
+        right_arr[idx] = (m * right_arr[idx + num_nodes] - (x_right - *node) * right_arr[idx]) / c1;
+      } while (m != 0);
+    }
+    c3 = c2;
+    if(current_M > M_repeats)
+    {
+      --current_M;
+    }
+  }
+  // return the actual difference of both vectors. 
+  auto end = std::next(right_arr.cbegin(), num_nodes * (M_repeats+1));
+  return std::transform(
+    right_arr.cbegin(), end,
+    left_arr.cbegin(), 
+    dest, 
+    [](const Scalar& r, const Scalar& l){ return r-l; }
+  );
+}
+
+  } // end namespace utils
+} // end namespace fornfdm 
 
 #endif
